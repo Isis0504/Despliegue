4b0b694487f59890aa87b70fc7d1b629f68201cc
Isis0504 <130944338+Isis0504@users.noreply.github.com>
2025-10-25 05:11:08 +0000

    s

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,9 +4,6 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const SUPABASE_URL = window.ENV?.SUPABASE_URL;
 const SUPABASE_ANON_KEY = window.ENV?.SUPABASE_ANON_KEY;
 
-// Verificación por si algo no está definido
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  console.error("❌ Error: variables de entorno de Supabase no definidas.");
-}
+
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
